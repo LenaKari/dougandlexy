8dd6fa35d8ae5794bb7854bdda0b037bd2ae4464
Douglas Ray <dpr980@gmail.com>
2016-09-15 14:25:15 -0400

    more changes to css on confirmation

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -4,11 +4,11 @@ import { Jumbotron } from 'react-bootstrap'
 export default class Confirmation extends React.Component {
   render() {
     return (
-      <div class="confirmation">
+      <Jumbotron class="confirmation">
         <h3>Your RSVP has been submitted</h3>
         <p>If you have any further questions, please contact Doug at <a href="mailto:douglas.paul.ray@gmail.com">douglas.paul.ray@gmail.com</a></p>
         <p>Thank you,<br/>We hope to see you there!</p>
-      </div>
+      </Jumbotron>
     );
   }
 }
