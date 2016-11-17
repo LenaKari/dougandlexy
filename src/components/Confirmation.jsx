cca7ac6b3f933d082a1f82f53fe586636ff05473
Douglas Ray <dpr980@gmail.com>
2016-11-17 13:44:25 -0500

    one last change

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -5,8 +5,8 @@ export default class Confirmation extends React.Component {
   render() {
     return (
       <Jumbotron class="confirmation">
-        <h3>Your request has been sent</h3>
-        <p>A link to the google photo album will be sent to you shortly</p>
+        <h3>Your request has been sent.</h3>
+        <p>A link to the google photo album will be sent to you shortly.</p>
       </Jumbotron>
     );
   }
